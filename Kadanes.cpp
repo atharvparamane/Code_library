358ee53c42b9987c3b30a5b4fc601278b39ea6e6
Atharv Paramane <atharvparamane111@gmail.com>
2021-06-20 00:05:09 +0530

    Update Kadanes.cpp

--- Kadanes.cpp
+++ Kadanes.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 void kadanealgo(int *arr,int n)
 {
-    ll cur_sum=0;
+    ll current_sum=0;
     ll soln=INT_MIN;
     for(int i=0;i<n;i++){
-        cur_sum+=arr[i];
-        soln=max(soln,cur_sum);
-        if(cur_sum<0){
-            cur_sum=0;
+        current_sum+=arr[i];
+        soln=max(soln,current_sum);
+        if(current_sum<0){
+            current_sum=0;
         }
     }
     cout<<soln<<"\n";
@@ -20,7 +20,8 @@ int main()
     int m;
     cin>>m;
     int arr[m+1];
-    for(int i=0;i<m;i++){
+    for(int i=0;i<m;i++)
+    {
         cin>>arr[i];
     }
     kadanealgo(arr,m);
