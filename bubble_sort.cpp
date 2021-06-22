0452f000319e1eac80c65dd2d1062deca191da67
Atharv Paramane <atharvparamane111@gmail.com>
2021-06-22 11:50:47 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+
+void bubbleSort(int arr[], int n)
+{
+int i, j;
+bool swapped;
+for (i = 0; i < n-1; i++)
+{
+	swapped = false;
+	for (j = 0; j < n-i-1; j++)
+	{
+		if (arr[j] > arr[j+1])
+		{
+		swap(&arr[j], &arr[j+1]);
+		swapped = true;
+		}
+	}
+
+	if (swapped == false)
+		break;
+}
+}
+
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("n");
+}
+
+// Driver program to test above functions
+int main()
+{
+	int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+	bubbleSort(arr, n);
+	printf("Sorted array: \n");
+	printArray(arr, n);
+	return 0;
+}
